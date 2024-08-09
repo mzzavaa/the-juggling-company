fe00d983395b5d7a47222cfaee3f30ab51bbec78
Linda M <56845556+mzzavaa@users.noreply.github.com>
2024-08-09 11:09:24 +0000

    refactor: Add logo to Header component

--- src/components/sections/Header/index.tsx
+++ src/components/sections/Header/index.tsx
@@ -7,21 +7,30 @@ import MuiBox from '@mui/material/Box';
 import MuiToolbar from '@mui/material/Toolbar';
 import MuiTypography from '@mui/material/Typography';
 
-export type Props = types.Header & types.StackbitObjectId;
+//export type Props = types.Header & types.StackbitObjectId;
+export type Props = types.Header & types.StackbitObjectId & {
+    logoSrc?: string;
+    logoAlt?: string;
+};
 
 export const Header: React.FC<Props> = (props) => {
-    const { title, navLinks = [], 'data-sb-object-id': objectId } = props;
+    const { title, navLinks = [], 'data-sb-object-id': objectId, logoSrc, logoAlt } = props;
     const fieldPath = objectId ? `${objectId}:header` : null;
     return (
         <MuiAppBar position="static" color="transparent" elevation={0} data-sb-field-path={fieldPath}>
             <MuiToolbar disableGutters={true} sx={{ flexWrap: 'wrap' }}>
-                {title && (
-                    <MuiBox sx={{ mb: 1, mr: 2, flexGrow: 1 }}>
-                        <MuiTypography component="p" variant="h6" color="text.primary" noWrap data-sb-field-path=".title">
-                            {title}
-                        </MuiTypography>
-                    </MuiBox>
-                )}
+                {logoSrc && (
+                        <MuiBox sx={{ mb: 1, mr: 2 }}>
+                            <img src={logoSrc} alt={logoAlt} style={{ height: '40px' }} data-sb-field-path=".logo" />
+                        </MuiBox>
+                    )}
+                    {title && (
+                        <MuiBox sx={{ mb: 1, mr: 2, flexGrow: 1 }}>
+                            <MuiTypography component="p" variant="h6" color="text.primary" noWrap data-sb-field-path=".title">
+                                {title}
+                            </MuiTypography>
+                        </MuiBox>
+                    )}
                 {navLinks.length > 0 && (
                     <MuiBox component="nav" sx={{ display: 'flex', flexWrap: 'wrap' }} data-sb-field-path=".navLinks">
                         {navLinks.map((link, index) => (
