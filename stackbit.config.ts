c23959c8ab190a58811401ab374a49c38499c522
Linda M <mzzavaa@gmail.com>
2025-04-13 13:29:16 +0200

    Refactor stackbit.config.ts to simplify configuration

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,30 +1,13 @@
-import { GitContentSource } from '@stackbit/cms-git';
 import { defineStackbitConfig } from '@stackbit/types';
-import { allModels } from './.stackbit/models';
 
-const gitContentSource = new GitContentSource({
-    rootPath: __dirname,
-    contentDirs: ['content'],
-    models: Object.values(allModels),
-    assetsConfig: {
-        referenceType: 'static',
-        staticDir: 'public',
-        uploadDir: 'images',
-        publicPath: '/'
-    }
+export default defineStackbitConfig({
+  stackbitVersion: '~0.6.0',
+  ssgName: 'nextjs',
+  nodeVersion: '18',
+  contentSources: [],
+  presetSource: {
+    type: 'files',
+    presetDirs: ['.stackbit/presets'],
+  },
+  styleObjectModelName: 'ThemeStyle',
 });
-
-export const sbConfig = defineStackbitConfig({
-    stackbitVersion: '~0.6.0',
-    ssgName: 'nextjs',
-    nodeVersion: '18',
-    contentSources: [gitContentSource],
-    presetSource: {
-        type: 'files',
-        presetDirs: ['.stackbit/presets']
-    },
-    styleObjectModelName: 'ThemeStyle'
-});
-
-export default sbConfig;
-
